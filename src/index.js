b11464fc3f3c99eee0002a6a05f5946b9d97a0f6
hjcanad <hjcanad@gmail.com>
2026-07-27 11:32:29 -0500

    added code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faShoppingCart, faTimes} from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(faBars, faShoppingCart, faTimes);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +16,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
