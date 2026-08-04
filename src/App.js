eca3ccd2ccaa0ffbc743e869b0d90d8bac07952d
hjcanad <hjcanad@gmail.com>
2026-08-04 18:41:26 -0500

    added code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import Nav from './components/Nav';
 import Footer from './components/Footer'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -6,16 +6,28 @@ import Home from './Pages/Home';
 import Books from './Pages/Books';
 import { books } from './data';
 import BookInfo from './Pages/BookInfo';
+import Cart from './Pages/Cart';
 
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+  useEffect(() => {
+    console.log(cart)
+  }, [cart])
+
   return (
     <Router> 
     <div className="App">
     <Nav />
     <Route path="/" exact component={Home} />
     <Route path="/books" exact render={() => <Books books={books} />} />
-    <Route path="/books/1" render={() => <BookInfo books={books} />} />
+    <Route path="/books/:id" render={() => <BookInfo books={books} addToCart={addToCart} />} />
+    <Route path="/cart" render={() => <Cart books={books} />} />
     <Footer />
     </div>
     </Router>
--- src/Pages/BookInfo.jsx
+++ src/Pages/BookInfo.jsx
@@ -1,10 +1,22 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import react from 'react';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import React, { useState } from 'react';
+import { Link, useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import Rating from '../components/ui/Rating';
 import Price from '../components/ui/Price';
+import Book from '../components/ui/Book';
 
-const BookInfo = ({ books }) => {
+const BookInfo = ({ books, addToCart, cart }) => {
+   const { id } = useParams();
+   const book = books.find((book) => +book.id === +id); 
+
+function addBookToCart(book) {
+    addToCart(book);
+}
+
+function bookExistsOnCart() {
+   return cart.find((book) => +book.id === +id);
+}
+
     return (
         <div id="books__body">
             <main id="books__main">
@@ -18,25 +30,56 @@ const BookInfo = ({ books }) => {
                                 <h2 className="book__selected--title--top">Books</h2>
                             </Link>
                         </div>
-
-                        <div className="book__selected">
+                         <div className="book__selected">
                             <figure className="book__selected--figure">
-                                <img src="" alt="" className="book__selected--img" />
+                                <img src={book.url} alt={book.title} className="book__selected--img" />
                             </figure>
-
-                            <div className="book__selected--description">
-                                <h2 className="book__selected--title">
-                                    Crack the Coding Interview: 189 Programming Questions and Solutions
-                                </h2>
-                                <Rating rating={4.5} />
+                         <div className="book__selected--description">
+                                <h2 className="book__selected--title">Crack the Coding Interview: 189 Programming Questions and Solutions</h2>
+                                <Rating rating={book.rating} />
                                 <div className="book__selected--price">
-                                    <Price originalPrice={50} salePrice={null} />
+                                    <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
                                 </div>
-                                <div className="book__summary"></div>
+                                <div className="book__summary">
+                                   <h3 className="book__summary--title">
+                                    Summary</h3>
+                                    <p className="book__summary--para">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                                    </p>
+                                    <p className="book__summary--para">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+                                    </p>
                             </div>
+                            {bookExistsOnCart() ? (
+                              <Link to={`/cart`} className="book__link">
+                            <button className="btn">Checkout</button>
+                            </Link>
+                            ) : ( 
+                              <button className="btn" onClick={() => addBookToCart(book)}>
+                                Add to Cart</button>
+                            )}
+                            
                         </div>
                     </div>
-                </div>
+                 </div>
+             </div>
+
+                <div className="books__container">
+                    <div className="row">
+                       <div className="book__selected--top">
+                        <h2 className="book__selected--title--top">
+                         Recommended Books</h2>
+                        </div>
+                        <div className="books">
+                        {books
+                        .filter((book) => book.rating === 5 && +book.id !== +id)
+                        .slice(0, 4)
+                        .map((book) => (
+                            <Book book={book} key={book.id} />
+                        ))}
+                        </div>
+                    </div>
+                </div>  
             </main>
         </div>
     );
